38c21e57aa6845001add734d8fd390f0b2969814
Jimmy Nguyen <9phuc.nguyen6@gmail.com>
2021-04-04 21:04:59 -0500

    updated for iterations

--- sketch_jan28a/sketch_jan28a.ino
+++ sketch_jan28a/sketch_jan28a.ino
@@ -13,6 +13,18 @@
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
+typedef struct BreathingTechnique {
+  unsigned short inhaleIterations;
+  unsigned short holdInterations;
+  unsigned short exhaleIterations;
+} BreathingTechnique;
+
+#define fse (BreathingTechnique) {.inhaleIterations = 4, .holdInterations = 7, .exhaleIterations = 8}
+#define fff (BreathingTechnique) {.inhaleIterations = 4, .holdInterations = 4, .exhaleIterations = 4}
+#define tst (BreathingTechnique) {.inhaleIterations = 1, .holdInterations = 1, .exhaleIterations = 1}
+
+#define cbt {fse, fff, tst}
+
 void setup() {
   Serial.begin(115200);
 
@@ -22,10 +34,11 @@ void setup() {
   }
   delay(1000);
 
+//  displayTitle();
+//  delay(5000);
 //  displayMenu();
-//  delay(3000);
-  displayAnim();
-  
+//  displayAnim();
+    startTechnique(tst);
 }
 
 void loop() {
@@ -45,7 +58,7 @@ void prepareDisplay() {
   display.setTextWrap(false);
 }
 
-void displayMenu() {
+void displayTitle() {
   String title = "BREATHE";
   String desc = "By Jimmy Nguyen";
   String ver = "V1.0";
@@ -54,20 +67,49 @@ void displayMenu() {
   display.drawFastHLine(0, 25, (SCREEN_WIDTH - 5), WHITE);
   setText(desc, 1, 22, 30);
   setText(ver, 1, (SCREEN_WIDTH / 2) - 10, 55);
+  display.display();
+}
+
+void displayMenu() {
+  prepareDisplay();
+
+  String techniqueName = String(cbt.inhaleIterations) + "-" + String(cbt.holdIteartions) + "-" + String(cbt.exhaleIterations);
+  setText(techniqueName, 2, 36, 0);
+  setText("Technique", 1, 39, 25);
+  display.drawFastHLine(0, 50, SCREEN_WIDTH, WHITE);
+  setText("Start", 1, 0, 55);
+  setText("Cycle", 1, (SCREEN_WIDTH - 29), 55);
   display.display(); 
 }
 
-void displayAnim() {
-  int counter = 0;
+void startTechnique(BreathingTechnique bt) {
+  int iterations = 0;
   for (;;) {
+    setText("Iteration: " + String(iterations++), 1, 46, 0);
+    display.display();
+    delay(1500);
+    displayTechnique(bt.inhaleIterations, "INHALE");
+    displayTechnique(bt.holdInterations, "-HOLD-");
+    displayTechnique(bt.exhaleIterations, "EXHALE");
+  }
+}
+
+void displayTechnique(unsigned short iterations, String type) {
+  int circleX = (SCREEN_WIDTH / 2);
+  int circleY = (SCREEN_HEIGHT / 2) - 10;
+  int circleRadius = 20;
+  for (int i = 1; i < iterations + 1; i++) {
     prepareDisplay();
-    display.drawCircle((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2) - 10, 20, WHITE);
-    int x = counter >= 10 ? (SCREEN_WIDTH / 2) - 5 : (SCREEN_WIDTH / 2) - 2;
-    setText(String(counter), 1, x, (SCREEN_HEIGHT / 2) - 10);
-    counter++;
+    setText(type, 1, 46, 55);
+    display.fillCircle(circleX, circleY, circleRadius, WHITE);
     display.display();
     delay(500);
-    display.fillCircle((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2) - 10, 20, WHITE);
+
+    prepareDisplay();
+    setText(type, 1, 46, 55);
+    display.drawCircle(circleX, circleY, circleRadius, WHITE);
+    int textXPosition = i >= 10 ? circleX - 5 : circleX - 2;
+    setText(String(i), 1, textXPosition, circleY - 2);
     display.display();
     delay(500);
   }
@@ -76,4 +118,5 @@ void displayAnim() {
 void findCenter() {
   display.setCursor((SCREEN_WIDTH / 2), (SCREEN_HEIGHT) - 10);
   display.println("x");
+  display.display();
 }
